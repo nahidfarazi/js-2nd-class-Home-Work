7b788684cb4bfd61bfd4a7f2403fd593bab445d2
nahidfarazi <nahidfarazi19@gmail.com>
2023-04-19 01:55:00 +0600

    2nd class home work done

--- filter.js
+++ filter.js
@@ -1,4 +1,6 @@
 const myArr = [23,34,33.67,35,789,678]
 
-const oneItem = myArr.filter((number) => number <= 400)
+const oneItem = myArr.filter((number) => number == 23)
+const moreItem = myArr.filter((number) => number >= 100)
 console.log(oneItem);
+console.log(moreItem);
